b955d3effbd188794889ea0c0297589e89aa8c3d
devhnry <devthenry@gmail.com>
2025-06-15 15:23:19 +0100

    refactor: updated navbar to use container component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,14 +1,16 @@
+import Container from "../layout/Container.tsx";
+import Logo from "./Logo.tsx";
+
 const Navbar = () => {
     return (
-        <nav className={`px-6 py-4 shadow-md flex items-center justify-between`}>
-            <div className={`flex gap-1 items-baseline font-medium`}>
-                <p className={`p-2 bg-black text-white text-2xl font-bold rounded-xl`}>CU</p>
-                <p className={`text-xl`}>Timetable</p>
-            </div>
-            <div>
-                <button className={`border-2 p-1.5 px-4 font-bold rounded-md text-[14px] cursor-pointer hover:scale-105 transition-all`}>Login</button>
-            </div>
-        </nav>
+        <Container className={`shadow-md`}>
+            <nav className={`py-4 flex items-center justify-between`}>
+                <Logo />
+                <div>
+                    <button className={`border-2 p-1.5 px-3 font-bold rounded-md text-[12px] cursor-pointer hover:scale-105 transition-all`}>Login</button>
+                </div>
+            </nav>
+        </Container>
     );
 };
 

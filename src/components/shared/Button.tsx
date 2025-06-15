5e26a9c85a9751a7931e42afd5adbcc47230cfdb
devhnry <devthenry@gmail.com>
2025-06-15 23:43:31 +0100

    chore(ui): created pages for otp verification

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -3,12 +3,14 @@ import {useNavigate} from 'react-router-dom';
 
 interface ButtonProps {
     text: string;
+    type?: "submit" | "reset" | "button";
+    disabled?: boolean;
     classname?: string;
     to?: string;
     onClick?: () => void;
 }
 
-const Button: FC<ButtonProps> = ({ text, classname = 'p-1.5 px-3 hover:scale-[105%]', to, onClick }) => {
+const Button: FC<ButtonProps> = ({ text, type, classname = 'p-1.5 px-3 hover:scale-[105%]', to, onClick }) => {
     const navigate = useNavigate();
     const handleClick = () => {
         if (to) { navigate(to);
@@ -16,7 +18,7 @@ const Button: FC<ButtonProps> = ({ text, classname = 'p-1.5 px-3 hover:scale-[10
     };
 
     return (
-        <button className={`border-[1px] ${classname} font-medium rounded-md text-[12px] cursor-pointer transition-all`}
+        <button type={type} className={`border-[1px] ${classname} font-medium rounded-md text-[12px] cursor-pointer transition-all`}
             onClick={handleClick} >
             {text}
         </button>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,11 +2,13 @@ import {useRoutes} from 'react-router-dom';
 import LandingPage from "../pages/LandingPage.tsx";
 import NotFound from "../pages/NotFoundPage.tsx";
 import LoginPage from "../pages/LoginPage.tsx";
+import VerifyOtpPage from "../pages/VerifyOtpPage.tsx";
 
 const AppRoutes = () => {
     return useRoutes([
         {path: '/', element: <LandingPage/>},
         {path: '/login', element: <LoginPage/>},
+        {path: '/verify-otp', element: <VerifyOtpPage />},
         { path: '*', element: <NotFound />}, // 404 fallback
     ]);
 };

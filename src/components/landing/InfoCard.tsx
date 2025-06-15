7d2809f193444e0bc42102feba8eb2cab7b2001b
devhnry <devthenry@gmail.com>
2025-06-15 15:24:34 +0100

    feat: updated InfoCard styling for responsiveness and wrapped up landing page

--- src/components/landing/InfoCard.tsx
+++ src/components/landing/InfoCard.tsx
@@ -5,15 +5,17 @@ type InfoCardProps = {
     icon: LucideIcon;
     title: string;
     description: string;
+    classname?: string
 }
 
-const InfoCard: React.FC<InfoCardProps> = ({icon: Icon, title, description}) => {
+const InfoCard: React.FC<InfoCardProps> = ({icon: Icon, title, description, classname}) => {
     return (
-        <div className={`bg-[#F8F9FA] shadow-md text-black w-[320px] rounded-lg flex flex-col gap-2 p-4`}>
-            <div className={`bg-black p-2 rounded-sm w-fit text-[18px] text-white`}>
+        <div className={`bg-[#F8F9FA] shadow-md text-black max-w-[320px] lg:max-w-[380px] rounded-lg flex flex-col gap-2 p-4 ${classname} 
+        hover:scale-[102%] hover:-translate-y-[5px] transition-all cursor-pointer`}>
+            <div className={`bg-black p-2 rounded-sm w-fit text-[18px] lg:text-[20px] text-white`}>
                 {<Icon />}
             </div>
-            <h3 className={`font-bold`}>{title}</h3>
+            <h3 className={`font-bold lg:text-[18px]`}>{title}</h3>
             <p className={`text-black/80 font-light`}>{description}</p>
         </div>
     );
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,38 +3,47 @@ import {cardInfoContent} from "../data/features.ts"
 import heroContent from "../data/hero.json";
 import InfoCard from "../components/landing/InfoCard.tsx";
 import TimetableGrid from "../components/landing/TimetableGrid.tsx";
+import Container from "../components/layout/Container.tsx";
 
 const LandingPage = () => {
     return (
         <main>
             <Navbar />
-
-            <section className={`my-10 px-6`}>
-                <section>
-                    <div className={`flex flex-col gap-2.5 pb-6`}>
-                        <h1 className={`hero-title pb-2`}>{heroContent.title}</h1>
-                        <p className={`text-black/85`}>{heroContent.description}</p>
-                        <div className={`mx-2 flex gap-3 items-center`}>
-                            <div className={`flex`}>
-                                {['CU','HD','DP'].map((item,index) => (
-                                    <div className={`border-white border bg-[#CCD0D7] rounded-full p-0.5 text-[10px] -mx-1 relative z-${index*10}`} key={item}>{item}</div>
-                                ))}
+            <section className={`my-10`}>
+                <Container>
+                    <section className={`flex flex-col lg:flex-row gap-10 xl:gap-16 items-center mx-auto w-full max-w-[1200px]`}>
+                        <div className={`flex flex-col gap-3 pb-6 max-w-[600px] w-full`}>
+                            <h1 className={`hero-title pb-2 leading-[100%] lg:leading-[110%]`}>{heroContent.title}</h1>
+                            <div className={`grid gap-8`}>
+                                <p className={`text-black/85 max-w-[490px] text-[18px]`}>{heroContent.description}</p>
+                                <div className={` w-fit grid grid-cols-2 gap-5`}>
+                                    <button className={`border-[1px] py-2 px-3 font-medium text-black rounded-md text-[18px] cursor-pointer hover:scale-[101%] transition-all`}>Generate Timetable</button>
+                                    <button className={`border-[1px] py-2 px-1.5 font-medium bg-black text-white rounded-md text-[18px] cursor-pointer hover:scale-[101%] hover:bg-black/90 transition-all`}>Login</button>
+                                </div>
+                                <div className={`mx-2 flex gap-3 items-center`}>
+                                    <div className={`flex`}>
+                                        {['CU','HD','DP'].map((item,index) => (
+                                            <div className={`border-white border bg-[#CCD0D7] rounded-full p-0.5 text-[12px] -mx-1 relative z-${index*10}`} key={item}>{item}</div>
+                                        ))}
+                                    </div>
+                                    <p className={`text-[14px] text-black tracking-tight`}>{heroContent.caption}</p>
+                                </div>
                             </div>
-                            <p className={`text-[11px] text-black font-medium tracking-tight`}>{heroContent.caption}</p>
                         </div>
-                    </div>
-                    <div>
-                        <TimetableGrid />
-                    </div>
-                </section>
+                        <div className={`hidden 2lg:block`}>
+                            <TimetableGrid />
+                        </div>
+                    </section>
 
-                <section>
-                    <div className={`grid items-center gap-8 mx-auto w-fit my-10`}>
-                        {cardInfoContent.map(info => {
-                            return <InfoCard key={info.title} {...info}/>
-                        })}
-                    </div>
-                </section>
+                    <section>
+                        <div className={`grid md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center mx-auto w-fit my-10 [&>*]:h-full`}>
+                            {cardInfoContent.map((info, index) => {
+                                return <InfoCard key={info.title} {...info}
+                                                 classname={`${index == 2 ? 'md:col-span-2 lg:col-span-1' : ''}`}/>
+                            })}
+                        </div>
+                    </section>
+                </Container>
             </section>
         </main>
     );
